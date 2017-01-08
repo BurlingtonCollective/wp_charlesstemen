049338e5d2177bc35c8933e056f575c7788701b8
Zachary Lincoln <znlincoln@gmail.com>
2017-01-08 03:50:12 -0500

    smoother video carousel

--- wp-content/themes/wp_charlesstemen/js/main.js
+++ wp-content/themes/wp_charlesstemen/js/main.js
@@ -1,22 +1,22 @@
 jQuery(function ($) {
-  $(document).ready(function () {
-    $('.wysiwyg-content').fitVids();
+    $(document).ready(function () {
+        $('.wysiwyg-content').fitVids();
 
-    $('.carousel-block')
-      .on('slid.bs.carousel', function (e) {
-        $(this).find('.carousel-index').text($(e.relatedTarget).index() + 1);
-      });
+        $('.carousel-block')
+            .on('slid.bs.carousel', function (e) {
+                $(this).find('.carousel-index').text($(e.relatedTarget).index() + 1);
+            });
 
-    $('.carousel-block-video')
-      .on('slide.bs.carousel', function (e) {
-        $.each($(this).find('iframe'), function () {
-          //pause video hack
-          var source = $(this).attr('src');
-          $(this).attr('src', '');
-          $(this).attr('src', source);
-        })
-      });
-
-
-  });
+        $('.carousel-block-video')
+            .on('slid.bs.carousel', function (e) {
+                $items = $(this).find('.item').not(e.relatedTarget);
+                $.each($items, function () {
+                    //pause video hack
+                    var $iframe = $(this).find('iframe');
+                    var source = $iframe.attr('src');
+                    $iframe.attr('src', '');
+                    $iframe.attr('src', source);
+                });
+            });
+    });
 });
