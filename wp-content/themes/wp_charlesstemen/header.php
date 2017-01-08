c6bf4ce67d5e984d6439d7212ac1d13a7bbb5277
Zachary Lincoln <znlincoln@gmail.com>
2017-01-08 04:47:10 -0500

    navbar collapse works + projects have padding bottom

--- wp-content/themes/wp_charlesstemen/header.php
+++ wp-content/themes/wp_charlesstemen/header.php
@@ -40,7 +40,7 @@ function dl_do_header()
     <header class="navbar navbar-default navbar-fixed-top" role="navigation">
         <div class="container">
             <div class="navbar-header">
-                <button type="button" class="navbar-toggle">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#main-nav">
                     <span class="sr-only">Toggle Navigation</span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
